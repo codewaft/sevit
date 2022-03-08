514c550eedfc38f02516515ebee1755769f5a5ee
rahulspace <rahultp518@gmail.com>
2022-03-08 20:51:24 +0530

    Set font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
     content: ["./resources/**/*.blade.php", "./resources/**/*.tsx"],
     theme: {
         extend: {},
+        fontFamily: {
+            sans: ["Fredoka", "sans-serif"],
+        },
     },
     plugins: [],
 };
