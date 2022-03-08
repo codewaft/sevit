40e26db45b80b10c446024ae700948295cc5455b
renjithspace <renjithrajeevvk@gmail.com>
2022-03-08 22:51:59 +0530

    Create message migration

--- database/migrations/2022_03_08_162816_create_messages_table.php
+++ database/migrations/2022_03_08_162816_create_messages_table.php
@@ -7,16 +7,17 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up()
     {
-        Schema::create("broadcast_messages", function (Blueprint $table) {
+        Schema::create("messages", function (Blueprint $table) {
+            $table->id();
             $table->foreignId("contact_id")->constrained();
             $table->string("message_id");
             $table->enum("status", ["scheduled", "processed", "failed"]);
-            $table->timestampTz("processed_at");
+            $table->timestampTz("processed_at")->nullable();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists("broadcast_messages");
+        Schema::dropIfExists("messages");
     }
 };
---
+++ database/migrations/2022_03_08_171814_create_broadcast_messages.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create("broadcast_messages", function (Blueprint $table) {
+            $table->foreignId("broadcast_id")->constrained();
+            $table->foreignId("message_id")->constrained();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists("broadcast_messages");
+    }
+};
