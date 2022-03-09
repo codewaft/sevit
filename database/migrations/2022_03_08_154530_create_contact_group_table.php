155e0dce1a309cb929707d0c9ec438367fc57c7f
renjithspace <renjithrajeevvk@gmail.com>
2022-03-09 09:35:42 +0530

    Rename contact_groups table to contact_group

--- database/migrations/2022_03_08_154530_create_contact_group_table.php
+++ database/migrations/2022_03_08_154530_create_contact_group_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up()
     {
-        Schema::create("contact_groups", function (Blueprint $table) {
+        Schema::create("contact_group", function (Blueprint $table) {
             $table->foreignId("contact_id")->constrained();
             $table->foreignId("group_id")->constrained();
         });
@@ -15,6 +15,6 @@ return new class extends Migration {
 
     public function down()
     {
-        Schema::dropIfExists("contact_groups");
+        Schema::dropIfExists("contact_group");
     }
 };
