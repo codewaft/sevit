bf839a73396206041c81c3b3f8ec40122c0e4198
renjithspace <renjithrajeevvk@gmail.com>
2022-03-09 09:49:17 +0530

    Add relationships

--- app/Models/Broadcast.php
+++ app/Models/Broadcast.php
@@ -10,4 +10,19 @@ class Broadcast extends Model
     use HasFactory;
 
     protected $fillable = ["title", "status", "scheduled_at"];
+
+    public function template()
+    {
+        return $this->belongsTo(Template::class);
+    }
+
+    public function groups()
+    {
+        return $this->belongsToMany(Group::class);
+    }
+
+    public function messages()
+    {
+        return $this->belongsToMany(Message::class);
+    }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -10,4 +10,14 @@ class Message extends Model
     use HasFactory;
 
     protected $fillable = ["reference_id", "status", "processed_at"];
+
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
+
+    public function broadcasts()
+    {
+        return $this->belongsToMany(Broadcast::class);
+    }
 }
