30ad7a2a82488ea09827287e68cfb1c19e699fb7
renjithspace <renjithrajeevvk@gmail.com>
2022-03-14 22:29:47 +0530

    Schedule queue:work every minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,26 +7,16 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule
+            ->command("queue:work --sleep=3 --tries=2 --max-time=60")
+            ->everyMinute();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
+        $this->load(__DIR__ . "/Commands");
+        require base_path("routes/console.php");
     }
 }
