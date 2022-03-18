86285e9deaeb38a24340b76dbf4f770406b3c759
renjithspace <renjithrajeevvk@gmail.com>
2022-03-18 17:14:16 +0530

    Rewrite request service to promise

--- resources/ts/services/request.service.ts
+++ resources/ts/services/request.service.ts
@@ -2,15 +2,13 @@ import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { forEach } from "lodash";
 import routes from "../routes";
 import storageService from "./storage.service";
-import alert from "./alert.service";
 import progressBar from "./progressBar.service";
+import alertService from "./alert.service";
 
 type ContentType = "json" | "form";
 type ContentMimes = Record<ContentType, string>;
 
-export interface ResponseCallback<ResponseData> {
-  (data: ResponseData | null): void;
-}
+type ReqReturn<ResponseData> = Promise<ResponseData | null>;
 
 export interface PaginateRequest {
   page: number;
@@ -72,7 +70,7 @@ export default {
     const unauthorized = response && response.status === 401;
     const requestError = response && response.data.message;
     const message = requestError || error.message;
-    alert.error(message);
+    alertService.error(message);
     if (unauthorized) {
       storageService.remove("authToken");
       window.location.href = routes.signIn;
@@ -80,77 +78,67 @@ export default {
     console.error(error);
   },
 
-  get<ResponseData>(
-    url: string,
-    params: object,
-    callback: ResponseCallback<ResponseData>
-  ) {
-    this.client()
-      .get<ResponseData>(url, { params })
-      .then((response) => callback(response.data))
-      .catch((error) => {
-        callback(null);
-        this.handleError(error);
-      });
+  get<Resp>(url: string, params: object): ReqReturn<Resp> {
+    return new Promise((resolve) => {
+      this.client()
+        .get<Resp>(url, { params })
+        .then((response) => resolve(response.data))
+        .catch((error) => {
+          resolve(null);
+          this.handleError(error);
+        });
+    });
   },
 
-  post<RequestData, ResponseData>(
-    url: string,
-    payload: RequestData,
-    callback: ResponseCallback<ResponseData>
-  ) {
-    this.client()
-      .post<ResponseData>(url, payload)
-      .then((response) => callback(response.data))
-      .catch((error) => {
-        callback(null);
-        this.handleError(error);
-      });
+  post<Req, Resp>(url: string, payload: Req): ReqReturn<Resp> {
+    return new Promise((resolve) => {
+      this.client()
+        .post<Resp>(url, payload)
+        .then((response) => resolve(response.data))
+        .catch((error) => {
+          resolve(null);
+          this.handleError(error);
+        });
+    });
   },
 
-  postForm<RequestData, ResponseData>(
-    url: string,
-    payload: RequestData,
-    callback: ResponseCallback<ResponseData>
-  ) {
-    const fields = payload as unknown as object;
-    const formData = new FormData();
-    forEach(fields, (value, key) => formData.append(key, value));
-    this.client("form")
-      .post<ResponseData>(url, formData)
-      .then((response) => callback(response.data))
-      .catch((error) => {
-        callback(null);
-        this.handleError(error);
-      });
+  postForm<Req, Resp>(url: string, payload: Req): ReqReturn<Resp> {
+    return new Promise((resolve) => {
+      const fields = payload as unknown as object;
+      const formData = new FormData();
+      forEach(fields, (value, key) => formData.append(key, value));
+      this.client("form")
+        .post<Resp>(url, formData)
+        .then((response) => resolve(response.data))
+        .catch((error) => {
+          resolve(null);
+          this.handleError(error);
+        });
+    });
   },
 
-  patch<RequestData, ResponseData>(
-    url: string,
-    payload: RequestData,
-    callback: ResponseCallback<ResponseData>
-  ) {
-    this.client()
-      .patch<ResponseData>(url, payload)
-      .then((response) => callback(response.data))
-      .catch((error) => {
-        callback(null);
-        this.handleError(error);
-      });
+  patch<Req, Resp>(url: string, payload: Req): ReqReturn<Resp> {
+    return new Promise((resolve) => {
+      this.client()
+        .patch<Resp>(url, payload)
+        .then((response) => resolve(response.data))
+        .catch((error) => {
+          resolve(null);
+          this.handleError(error);
+        });
+    });
   },
 
-  delete<RequestData, ResponseData>(
-    url: string,
-    payload: RequestData,
-    callback: ResponseCallback<ResponseData>
-  ) {
-    const config = { data: payload };
-    this.client()
-      .delete<ResponseData>(url, config)
-      .then((response) => callback(response.data))
-      .catch((error) => {
-        callback(null);
-        this.handleError(error);
-      });
+  delete<Req, Resp>(url: string, payload: Req): ReqReturn<Resp> {
+    return new Promise((resolve) => {
+      const config = { data: payload };
+      this.client()
+        .delete<Resp>(url, config)
+        .then((response) => resolve(response.data))
+        .catch((error) => {
+          resolve(null);
+          this.handleError(error);
+        });
+    });
   },
 };
