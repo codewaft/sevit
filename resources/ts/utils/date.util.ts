9e46a32bf00928d038894d348e0553e6db018af9
renjithspace <renjithrajeevvk@gmail.com>
2022-04-01 21:34:53 +0530

    Fix: processed_at showing Invalid Date Time

--- resources/ts/utils/date.util.ts
+++ resources/ts/utils/date.util.ts
@@ -1,7 +1,8 @@
 import { DateTime } from "luxon";
 
 export default {
-  humanize(date: string) {
+  humanize(date: string | null) {
+    if (date === null) return null;
     const dateObject = DateTime.fromISO(date);
     return dateObject.toFormat("DD h:m a");
   },
