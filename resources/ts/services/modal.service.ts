9f02228f2cd38d412362cb65ec280eee43b8040a
renjithspace <renjithrajeevvk@gmail.com>
2022-04-01 22:06:27 +0530

    Clean up

--- resources/ts/services/modal.service.ts
+++ resources/ts/services/modal.service.ts
@@ -1,5 +1,4 @@
 import store from "../store/store";
-import { replaceRoute } from "../components/AutoNavigate/AutoNavigate.slice";
 import { resetActive as resetActiveModal } from "../components/Modals/Modals.slice";
 
 export default {
