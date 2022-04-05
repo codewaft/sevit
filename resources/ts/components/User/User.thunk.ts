b075a64139cd21c363838718dbfcc565ed203fab
renjithspace <renjithrajeevvk@gmail.com>
2022-04-05 18:05:14 +0530

    Remove confirm when signOut #19

--- resources/ts/components/User/User.thunk.ts
+++ resources/ts/components/User/User.thunk.ts
@@ -1,14 +1,17 @@
 import routes from "../../routes";
 import storageService from "../../services/storage.service";
-import autoNavigateService from "../../services/autoNavigate.service";
 import userApi from "../../apis/user.api";
+import store from "../../store/store";
+import { replaceRoute } from "../AutoNavigate/AutoNavigate.slice";
+import { removeConfirm } from "../Confirm/Confirm.slice";
 
 export function signOut() {
   return async () => {
     const isSignedOut = await userApi.signOut();
     if (isSignedOut) {
       storageService.remove("authToken");
-      autoNavigateService.push(routes.signIn);
+      store.dispatch(replaceRoute(routes.signIn));
+      store.dispatch(removeConfirm());
     }
   };
 }
